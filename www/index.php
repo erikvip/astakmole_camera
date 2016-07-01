59634641b97e190a636850830606d728b011e159
Erik Phillips <erikvip@gmail.com>
2016-07-01 07:21:13 -0600

    Style changes for web interface, format dates correclty, adjusted desktop layout

--- www/index.php
+++ www/index.php
@@ -3,6 +3,7 @@
     # Grab a list of all available videos
     $videos = `find ./data/ -iname '*.mp4'`;
     $videos = explode("\n", $videos);
+    
 
     $events = array();
     $compilation = array();
@@ -22,11 +23,13 @@
         }
 
         list($day, $event, $time) = explode("_", strstr($file, '.', true));
+        $day_ts = DateTime::createFromFormat("Ymd", $day)->format('U');
         if ($event === 'All') {
             $compilation["{$day}"] = array(
                 'event_number' => 0, 
                 'file' => $file, 
                 'day' => $day,
+                'day_ts' => $day_ts,
                 'start_time' => '000000', 
                 'end_time' => '115959'
             );
@@ -44,19 +47,26 @@
         }
         $counts["{$day}"]++;
 
+        
+        $start_ts = DateTime::createFromFormat("Ymd His", "{$day} {$start_time}")->format('U');
+        $end_ts = DateTime::createFromFormat("Ymd His", "{$day} {$end_time}")->format('U');
+
         $events["{$day}"]["{$event_number}"] = array(
             'event_number' => $event_number, 
             'day' => $day, 
+            'day_ts' => $day_ts,
             'file' => $file, 
             'start_time' => $start_time, 
+            'start_time_ts' => $start_ts,
             'end_time' => $end_time, 
+            'end_time_ts' => $end_ts,
             'filesize' => filesize($file_path),
             'filepath' => $file_path
         );
     }
 
     //Now fix the sorting on all the event days
-    ksort($events);
+    krsort($events);
     foreach($events as $k=>$v) {
         ksort($events["$k"]);
     }
@@ -75,11 +85,15 @@
         ";
 
         foreach($day as $number=>$e) {
+            $fmt = "g:i:s a";
+            $start = date($fmt, $e['start_time_ts']);
+            $end = date($fmt, $e['end_time_ts']);
+
             $html .= "
                 <tr>
                     <td><a class='show-video' href='javascript:void(0);' data-file='{$e['filepath']}' title='{$e['file']}'>{$e['file']}</a></td>
                     <td>{$number}</td>
-                    <td>{$e['start_time']} - {$e['end_time']}</td>
+                    <td>{$start} - {$end}</td>
                     <td>" . human_filesize($e['filesize']) . "</td>
                 </tr>
             ";
@@ -97,10 +111,14 @@
         width: 640px;
         height: 480px;
     }
+    .desktop #live {
+        float: left;
+        margin: 12px 0 0 0;
+    }
     table.events {
         width: 100%;
     }
-    #video {
+    body.phone #video {
         display: none;
         position: fixed;
         z-index: 10000;
@@ -114,6 +132,14 @@
         height: 240px;
         width: 320px;
     }
+
+    body.desktop #video {
+        float: left;
+        height: 480px;
+        width: 640px;
+        margin: 12px 40px;
+    }
+
     body.dim #video {
         display: block;
     }
@@ -124,19 +150,47 @@
     body.dim #page {
         opacity: 0.4;
     }
+
+    ul#camera-dates {
+        list-style-type: none;
+        clear: both;
+    }
+    ul#camera-dates li {
+        border: 1px solid #333;
+        margin: 4px 0;
+        padding: 4px 16px;
+        width: 75%;
+    }
+    ul#camera-dates li a {
+        color: #111;
+        text-decoration: none;
+    }
+    ul#camera-dates li a:hover {
+        text-decoration: underline;
+        color: #333;
+    }
 </style>
 <body>
 <div id="page">
+
+    <div id="video">
+        <video width="320" height="240" controls>
+          <source src="data/20151214/output/20151214_All_Events.mp4" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+    </div>
+
     <div id="live">
         <img src="live.jpg.php" alt="Live feed" />
+        <p id="livestatus">Live Image</p>
     </div>
-    <p id="livestatus">Live Image</p>
 
     <ul id="camera-dates">
         <?php
-            foreach($events as $day=>$e) {
+             foreach($events as $day=>$e) {
                 $total = count($e);
-                $title = "{$day} - ${total} events";
+                $day_ts = $compilation["$day"]['day_ts'];
+                $title = "<b>{$day}</b> - " . date("D, F jS Y", $day_ts) . " - ${total} events";
                 echo "
                     <li>
                         <a href='javascript:void(0);' title='{$title}'>{$title}</a>
@@ -149,15 +203,35 @@
     </ul>
 </div>
 
-<div id="video">
-    <video width="320" height="240" controls>
-      <source src="data/20151214/output/20151214_All_Events.mp4" type="video/mp4">
-      Your browser does not support the video tag.
-    </video>
-</div>
 
 <script>
-    
+
+    addBodyCls = function(className) {
+        var b = document.body;
+        if (b.classList)
+            b.classList.add(className);
+        else
+            b.className += ' ' + className;
+
+    }
+   
+    detect_mobile = function() {
+        var isPhone = function() {
+          var check = false;
+          (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+          return check;
+        }
+
+        if (!isPhone()) {
+            addBodyCls('desktop');
+            window.isPhone = false;
+        } else {
+            addBodyCls('phone');
+            window.isPhone = true;
+        }
+    };
+    ready(detect_mobile);
+
     update_live_image = function() {
         live=document.getElementById('live');
         liveimg = live.children[0];
@@ -170,9 +244,9 @@
 
             window.setInterval(function() {
                 var d = new Date();
-                var time = d.getHours() + '-' + d.getMinutes() + '-' + d.getSeconds();
+                var time = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
                 liveimg.src = src + "?r=" + d.getTime();
-                st.innerHTML='Updated at ' + time;
+                //st.innerHTML='Updated at ' + time;
             }, 500);
 
         }
@@ -185,8 +259,9 @@
         li = ul.querySelectorAll('li');
         Array.prototype.forEach.call(li, function(el, i){
             link = el.querySelectorAll('a')[0];
-            div = el.querySelectorAll('div.day')[0];
+            
             link.addEventListener('click', function() {
+                div = el.querySelectorAll('div.day')[0];    
                 div.style.display = (div.style.display=='none') ?  '' : 'none';
             })
         });
@@ -210,25 +285,29 @@
     }
 
     function show_video(el) {
+        width = 320;
+        height = 240;
         container = document.getElementById('video');
-
         video = container.querySelectorAll('video source')[0];
-
-
         f = el.dataset.file;
-        html = '<video width="320" height="240" controls><source src="'+f+'" type="video/mp4"> Your browser does not support the video tag.</video>';
+
+        if (!window.isPhone) {
+            width = 640;
+            height = 480;
+        }
+        html = '<video width="'+width+'" height="'+height+'" controls autoplay><source src="'+f+'" type="video/mp4"> Your browser does not support the video tag.</video>';
         //video.innerHTML = video;
         container.innerHTML=html;
-        //video.src = f;
         b = document.body;
-
-        console.log(video);
-
-        className='dim';
-        if (b.classList)
-            b.classList.add(className);
-        else
-            b.className += ' ' + className;
+        
+       
+        if (window.isPhone) {
+            className='dim';
+            if (b.classList)
+                b.classList.add(className);
+            else
+                b.className += ' ' + className;
+        }
 
     }
 
